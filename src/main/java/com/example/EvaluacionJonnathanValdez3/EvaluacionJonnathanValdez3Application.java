40029e3133f458d09e32fd6ff5f33e53b89f8a01
Jonnathan Valdez <jonathanandresvaldezllivisaca@gmail.com>
2023-11-01 11:00:46 -0500

    Manejo de errores cuando el otro microservicio no responda

--- src/main/java/com/example/EvaluacionJonnathanValdez3/EvaluacionJonnathanValdez3Application.java
+++ src/main/java/com/example/EvaluacionJonnathanValdez3/EvaluacionJonnathanValdez3Application.java
@@ -2,6 +2,7 @@ package com.example.EvaluacionJonnathanValdez3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 public class EvaluacionJonnathanValdez3Application {
--- src/main/java/com/example/EvaluacionJonnathanValdez3/Posts/Services/ExternalService.java
+++ src/main/java/com/example/EvaluacionJonnathanValdez3/Posts/Services/ExternalService.java
@@ -10,11 +10,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -28,20 +33,45 @@ public class ExternalService {
     private  String externalServiceUrl;
 
     @PostConstruct
-    public void getPostsFromExternalService(){
-        try {
-            ResponseEntity<List<Post>> response = restTemplate.exchange(
-                    externalServiceUrl+"/posts",
-                    HttpMethod.GET,
-                    null,
-                    new ParameterizedTypeReference<List<Post>>() {
+    public void getPostsFromExternalService() {
+        int maxRetries = 3;  // Número máximo de reintentos
+        int retryIntervalMillis = 1000;  // Intervalo de reintento en milisegundos
+
+        for (int retry = 0; retry < maxRetries; retry++) {
+            try {
+                ResponseEntity<List<Post>> response = restTemplate.exchange(
+                        externalServiceUrl + "/posts",
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<List<Post>>() {
+                        }
+                );
+                List<Post> postsFromExternalService = response.getBody();
+                List<Post> existingPosts = postRepository.findAll();  // Obtener todos los posts existentes en la base de datos local
+
+                // Filtrar solo los posts nuevos que no están en la base de datos local
+                List<Post> newPosts = postsFromExternalService.stream()
+                        .filter(post -> !existingPosts.contains(post))
+                        .collect(Collectors.toList());
+
+                // Guardar los nuevos posts en la base de datos local
+                postRepository.saveAll(newPosts);
+
+                logger.info("Se han agregado " + newPosts.size() + " nuevos posts desde el otro microservicio.");
+                return;  // Éxito, no es necesario volver a intentar
+            } catch (RestClientException e) {
+                if (retry < maxRetries - 1) {
+                    // Si no es el último intento, esperar antes de volver a intentar
+                    try {
+                        Thread.sleep(retryIntervalMillis);
+                    } catch (InterruptedException ex) {
+                        Thread.currentThread().interrupt();
                     }
-            );
-            List<Post> posts = response.getBody();
-            postRepository.saveAll(posts);
-            logger.info("Los post han sido creados desde el otro microservicio");
-        }catch (RestClientException e){
-            e.printStackTrace();
+                } else {
+                    // Si es el último intento, registrar el error
+                    logger.error("Error al obtener posts del servicio externo en el último intento.", e);
+                }
+            }
         }
     }
 }
